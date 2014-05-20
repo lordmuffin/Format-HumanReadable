8dcf3474bfc1da0f769f94c80333f8161536b5bb
Andrew Jackson <dorkmeisterx69@gmail.com>
2014-05-19 21:50:07 -0500

    Update Format-HumanReadable.ps1

--- Format-HumanReadable.ps1
+++ Format-HumanReadable.ps1
@@ -1,5 +1,15 @@
 ### Format-HumanReadable.ps1 ###
 
-Function Format-HumanReadable () {
-
-}
+Function Format-HumanReadable 
+        {
+            param ($size)
+            switch ($size) 
+            {
+                {$_ -ge 1PB}{"{0:#.#'P'}" -f ($size / 1PB); break}
+                {$_ -ge 1TB}{"{0:#.#'T'}" -f ($size / 1TB); break}
+                {$_ -ge 1GB}{"{0:#.#'G'}" -f ($size / 1GB); break}
+                {$_ -ge 1MB}{"{0:#.#'M'}" -f ($size / 1MB); break}
+                {$_ -ge 1KB}{"{0:#'K'}" -f ($size / 1KB); break}
+                default {"{0}" -f ($size) + "B"}
+            }
+        }
